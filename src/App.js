51861c451ce4fb0e40add9332b9fce661f151802
evondev <trananhtuan400@gmail.com>
2022-06-01 23:40:34 +0700

    Install redux saga

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import { useEffect } from "react";
 import HackerNews from "./components/HackerNews";
 
 function App() {
+  // Generator function
+  // useEffect(() => {
+  //   function* demoGenerator() {
+  //     console.log("running");
+  //     let id = 1;
+  //     while (true) {
+  //       yield id;
+  //       id++;
+  //     }
+  //   }
+  //   const gen = demoGenerator();
+  //   console.log(gen.next());
+  //   console.log(gen.next());
+  //   console.log(gen.throw(new Error("error")));
+  // }, []);
   return (
     <div>
       <HackerNews></HackerNews>
--- src/redux-toolkit/configureStore.js
+++ src/redux-toolkit/configureStore.js
@@ -2,7 +2,10 @@ import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 import counterSlice, { increment, incrementByValue } from "./counterSlice";
 import globalSlice from "./globalSlice";
+import createSagaMiddleware from "@redux-saga/core";
 
+// create the saga middleware
+const sagaMiddleware = createSagaMiddleware();
 const reducer = combineReducers({
   counter: counterSlice,
   global: globalSlice,
@@ -24,13 +27,14 @@ const reducer = combineReducers({
 // };
 const store = configureStore({
   reducer,
-  middleware: (gDM) => gDM().concat(logger),
+  middleware: (gDM) => gDM().concat(logger, sagaMiddleware),
 });
 // store.subscribe(() => {
 //   //  javascript observer pattern
 //   console.log(`current state: ${store.getState().counter.count}`);
 // });
-store.dispatch(incrementByValue(1));
+// store.dispatch(incrementByValue(1));
 // store.dispatch(incrementByValue(3));
 // store.dispatch(incrementByValue(5));
+// sagaMiddleware.run(rootSaga);
 export default store;
