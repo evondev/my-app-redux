164153a0da10d3cdfdeeb0d49a2336359b40d190
evondev <trananhtuan400@gmail.com>
2022-06-03 00:46:41 +0700

    optimize

--- src/sagas/news/handlers.js
+++ src/sagas/news/handlers.js
@@ -1,11 +1,15 @@
-import { call, put } from "redux-saga/effects";
+import { call, put, select } from "redux-saga/effects";
 import { setLoading, setNews } from "./newsSlice";
 import requestGetNews from "./requests";
-
+// JWT
 export default function* handleGetNews({ payload, type }) {
+  let query = "";
   try {
+    const newsQuery = yield select((state) => state.news.query);
+    if (newsQuery === "") query = "react";
+    else query = payload;
     yield put(setLoading(true));
-    const response = yield call(requestGetNews, payload);
+    const response = yield call(requestGetNews, query);
     const { hits } = response.data;
     yield put(setNews(hits));
     yield put(setLoading(false));
